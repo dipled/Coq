b1ef74caada3fc3bd42891c4c3e91c77d9aa86ca
Pedro <pedrovargastannuri@gmail.com>
2023-04-18 17:42:34 -0300

    Chega

--- Teste.v
+++ Teste.v
@@ -1,31 +1,29 @@
 Require Import Coq.Arith.PeanoNat.
 Require Import Arith.
-From Coq Require Export String.]
+From Coq Require Export String.
 Inductive bool :Type :=
   |true
   |false.
-Notation "⊥" := false.
-Notation "⊤" := true.
-Definition and (b1:bool) (b2:bool) : bool :=
+
+Definition andb (b1:bool) (b2:bool) : bool :=
   match b1, b2 with
-  |⊤, ⊤ => ⊤
-  |_, _ => ⊥
+  |true, true => true
+  |_, _ => false
   end.
-Notation "p ∧ q" := (and p q) (at level 40, left associativity).
-Definition notb (b:bool) : bool :=
+Definition neg (b:bool) : bool :=
   match b with
   |false => true
   |true => false
   end.
-Definition or (b1:bool) (b2:bool) : bool :=
+Definition orb (b1:bool) (b2:bool) : bool :=
   match b1 with
   |false => b2
   |true => true
   end.
-Infix "∨" := or (at level 20, left associativity).
-Notation "¬" := notb (at level 10).
 
-Theorem dem_1: forall a b: bool, (notb (a ∨ b)) = (¬a ∧ ¬b).
+
+
+Theorem dem_1: forall a b: bool, (neg (orb a  b)) = andb (neg a) (neg b).
 Proof.
 intros [] [].
 -simpl. reflexivity.
@@ -59,3 +57,76 @@ induction a as [|a' IHa'].
 - simpl. rewrite <- n_plus_0. reflexivity.
 - simpl. rewrite IHa'. rewrite plus_succ. reflexivity.
 Qed.
+
+Theorem n_times_0: forall n, n * 0 = 0.
+Proof.
+intros n. induction n. 
+-reflexivity.
+-simpl. rewrite -> IHn. reflexivity.
+Qed.
+
+Theorem n_times_1: forall n, n * 1 = n.
+Proof.
+  intros b.
+  induction b.
+  - simpl. reflexivity.
+  - simpl. rewrite IHb. reflexivity.
+Qed.
+Theorem plus_n_1_succ_n: forall n, n + 1 = S n.
+Proof.
+  intros n.
+  induction n.
+  - simpl. reflexivity.
+  - simpl. rewrite IHn. reflexivity.
+Qed.
+Theorem plus_1_n_succ_n: forall n, 1 + n = S n.
+  Proof.
+    intros n.
+    induction n.
+    - simpl. reflexivity.
+    - simpl. reflexivity.
+  Qed.
+Theorem plus_one_ass: forall a b, 1 + (a + b) = (a + 1 + b).
+Proof.
+  intros a b.
+  induction a.
+  - simpl. reflexivity.
+  - simpl. rewrite <- plus_n_1_succ_n. 
+  rewrite <- plus_n_1_succ_n. 
+  rewrite -> plus_n_1_succ_n.
+  rewrite <- pluscom. rewrite IHa. reflexivity.
+Qed.
+Theorem plus_1_n_n_1: forall n, 1 + n = n + 1.
+Proof.
+  intros n. induction n.
+  - reflexivity.
+  - rewrite plus_n_1_succ_n. rewrite <- plus_n_1_succ_n. rewrite -> plus_n_1_succ_n. reflexivity.
+Qed.
+Theorem one_plus_sum: forall a b, 1 + (a+b) = (1 + a + b).
+Proof.
+  intros a c.
+  induction a.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+Theorem plusass: forall a b c, a + (b + c) = b + (a + c).
+Proof.
+  intros a b c. induction a.
+  -simpl. reflexivity.
+  -simpl. rewrite IHa. rewrite plus_succ. reflexivity. 
+Qed.
+Theorem multdist: forall a b : nat,  b + b * a = b * S a.
+Proof.
+  intros a b. induction b.
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHb. rewrite plusass. reflexivity.
+Qed.
+Theorem multcom: forall a b, a * b = b * a.
+Proof.
+  intros a b. 
+  induction a.
+  -simpl. rewrite n_times_0. reflexivity.
+  -simpl. rewrite <- multdist. rewrite IHa. reflexivity. 
+  
+Qed.
+
