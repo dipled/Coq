839f6ef53d9d1cf56297170a5b5ba8a69237e4d9
Pedro <pedrovargastannuri@gmail.com>
2023-04-29 11:06:16 -0300

    finalmente asuhdhsadh

--- LogicalFoundations/Induction.v
+++ LogicalFoundations/Induction.v
@@ -1,199 +1,198 @@
 
 From Coq Require Export String.
-Theorem mul_0_r : forall n:nat,
+Require Import Coq.Init.Nat.
 
-  n * 0 = 0.
+(* Teoremas do Induction.v e Basics.v*)
+
+Theorem add_0_r : forall n:nat, n + 0 = n.
 Proof.
-  intros n. induction n as [|n' IHn'].
-  -reflexivity.
-  -simpl. rewrite IHn'. reflexivity.
-Qed.
-Theorem plus_n_Sm : forall n m : nat,
-  S (n + m) = n + (S m).
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite -> IHn'. reflexivity.  Qed.
+
+Theorem add_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
 Proof.
-  intros. induction n.
-  -simpl. reflexivity.
-  -simpl. rewrite -> IHn. reflexivity.
+  intros n m p.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite IHn.
+    reflexivity.
 Qed.
-Theorem plus_n_Sm2 : forall n m : nat,
-  S (n + m) = S n + m.
-Proof.
-  intros. induction n.
-  -simpl. reflexivity.
-  -simpl. rewrite -> IHn. reflexivity.
-Qed.
-Theorem add_zero : forall n : nat,
-  n = n + 0.
-Proof.
-  intros. induction n.
-  -reflexivity.
-  -simpl. rewrite <- IHn. reflexivity.
-Qed.
+
 Theorem add_comm : forall n m : nat,
   n + m = m + n.
 Proof.
-  intros. induction n.
-  -simpl. rewrite <- add_zero. reflexivity.
-  -simpl. rewrite -> IHn. rewrite -> plus_n_Sm. reflexivity.
-Qed. 
-Theorem n_times_0: forall n, n * 0 = 0.
-Proof.
-intros n. induction n. 
--reflexivity.
--simpl. rewrite -> IHn. reflexivity.
-Qed.
-Lemma add_assoc : forall n m p : nat,
-  n + (m + p) = (n + m) + p.
-Proof.
-  intros. induction n as [|n' IHn'].
-  +simpl. reflexivity.
-  +simpl. rewrite <- IHn'. reflexivity.
-Qed.
-Lemma add_assoc2: forall a b c,
-  a + (b + c) = b + (a + c).
-Proof.
-  intros. induction a.
-  - simpl. reflexivity.
-  - simpl. rewrite IHa. rewrite plus_n_Sm. reflexivity.
-Qed.
-Fixpoint double (n:nat) :=
-  match n with
-  | O => O
-  | S n' => S (S (double n'))
-  end.
-
-Lemma double_plus : forall n, double n = n + n .
-Proof.
-  intros n.
+  intros n m.
   induction n.
-  *simpl. reflexivity.
-  *simpl. rewrite -> IHn. rewrite <- plus_n_Sm. reflexivity.
+  - simpl.
+    rewrite add_0_r.
+    reflexivity.
+  - simpl.
+    rewrite <- plus_n_Sm.
+    rewrite IHn.
+    reflexivity.
 Qed.
-Fixpoint even (n:nat) : bool  := 
-  match n with
-  |O => true
-  |S O => false
-  |S (S n') => even n'
-  end.
-(*O Lemma a seguir sera usado para provar o Teorema even_S*)
-Lemma double_negb: forall b : bool,
-  b = negb(negb b).
+
+Theorem add_shuffle3 : forall n m p : nat,
+  n + (m + p) = m + (n + p).
 Proof.
-  intros [].
-  -simpl. reflexivity. 
-  -simpl. reflexivity.
-Qed.
-Theorem even_S : forall n : nat,
-  even (S n) = negb (even n).
-Proof.
-  intros. induction n as [|k IHk].
-  +simpl. reflexivity.
-  +rewrite -> IHk. 
-  simpl. 
-  rewrite <- double_negb. 
+  intros n m p.
+  rewrite add_assoc.
+  assert(H: n + m = m + n). 
+    {rewrite add_comm. reflexivity. }
+  rewrite H.
+  rewrite add_assoc.
   reflexivity.
 Qed.
 
-Theorem mult_0_plus' : forall n m : nat,
-  (n + 0 + 0) * m = n * m.
+Lemma mult_n_Sm:
+  forall m n: nat,
+  n * S m = n + (n * m).
 Proof.
-  intros.
-  assert (H: n + 0 + 0 = n).
-  - rewrite add_zero. rewrite add_zero. reflexivity.
-  - rewrite H. reflexivity.
-Qed.
-
-Theorem plus_assoc: forall a b, a + b = b + a.
-Proof.
-  intros. induction a as [|k IHk].
-  - rewrite <- add_zero. simpl. reflexivity.
-  - simpl. rewrite IHk. rewrite plus_n_Sm. reflexivity.
-Qed.
-
-Theorem succ_n_plus_1: forall n, n + 1 = S n.
-Proof.
+  intros m n.
   induction n.
-  -reflexivity.
-  - simpl. rewrite IHn. reflexivity.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite IHn.
+    rewrite add_shuffle3.
+    reflexivity.
 Qed.
 
+Theorem mul_0_r : forall n:nat,
+  n * 0 = 0.
+Proof.
+  induction n as [ | n' IHn].
+  - reflexivity.
+  - simpl.
+    rewrite IHn.
+    reflexivity. 
+Qed.
+
+Theorem mul_comm : forall m n : nat,
+  m * n = n * m.
+Proof.
+  intros m n.
+  induction m.
+  - rewrite mul_0_r.
+    reflexivity.
+  - simpl.
+    rewrite mult_n_Sm.
+    rewrite IHm.
+    reflexivity.
+Qed.
+
+(*Fim dos Teoremas*)
+
+Fixpoint div2 (n:nat) : nat :=
+  match n with
+  | O => O
+  | S O => O 
+  | S (S n') => S (div2 n')  
+end.  
+
+Fixpoint sum (n : nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n + sum n'
+  end.
+
+Theorem plus_n_1 : forall (n : nat),
+  n + 1 = S (n).
+Proof.
+  intros n.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite IHn.
+    reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall (n m:nat),
+  n + S m = S (n + m).
+Proof. 
+  intros n m.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite IHn.
+    reflexivity.  
+Qed.
+
+Theorem mult_2_n_plus : forall (n : nat),
+  n + n = 2 * n.
+Proof.
+  intros n.
+  induction n.
+  - reflexivity.
+  - simpl.
+    rewrite add_0_r.
+    reflexivity.
+Qed.
+
+Theorem mul2_div2 : forall n : nat,
+  n = div2 (2 * n).
+Proof.
+  intros n.
+  induction n.
+  - reflexivity.
+  - rewrite mult_n_Sm.
+    simpl.
+    rewrite add_0_r.
+    rewrite mult_2_n_plus.
+    rewrite <- IHn.
+    reflexivity.
+Qed.
+
+Theorem div2_mult2_plus: forall (n m : nat),
+  n + div2 m = div2 (2 * n + m).
+Proof.
+  intros n m.
+  induction n.
+  - reflexivity.
+  - rewrite mult_n_Sm.
+    simpl.
+    rewrite add_0_r.
+    rewrite mult_2_n_plus.
+    rewrite <- IHn.
+    reflexivity.
+Qed.
+
+Theorem mult_Sn_m : forall (n m : nat),
+  S n * m = m + n * m.
+Proof.
+  intros n m.
+  rewrite mul_comm.
+  rewrite mult_n_Sm.
+  rewrite <- mul_comm.
+  reflexivity.
+Qed.
+
+Theorem sum_Sn : forall n : nat,
+  sum (S n) = S n + sum n.
+Proof.
+  reflexivity.
+Qed.
 Theorem mult_dist: forall a b, a*(b+1) = a*b + a.
 Proof.
   intros. induction a.
   - simpl. reflexivity.
   - simpl. rewrite <- add_assoc. rewrite IHa. simpl.
-  rewrite plus_n_Sm. rewrite add_assoc.
+  rewrite plus_n_Sm. rewrite add_assoc. rewrite <- plus_n_Sm.
   reflexivity.
 Qed.
-
-Lemma aux': forall a b, b + b * a = b * S a.
-Proof.
-  intros; induction a.
-  - rewrite n_times_0. replace (b*1) with (b).
-    rewrite <- add_zero. reflexivity.
-    induction b.
-    + reflexivity.
-    + simpl. rewrite <- IHb. reflexivity.
-  - rewrite <- succ_n_plus_1. 
-  rewrite -> mult_dist. rewrite add_assoc. rewrite IHa.
-  rewrite plus_n_Sm2. rewrite mult_dist. reflexivity.
-Qed.
-  
-Theorem mult_com: forall a b, a * b = b * a.
-Proof.
-  intros. induction a.
-  - simpl; rewrite mul_0_r. reflexivity.
-  - simpl.
-    rewrite plus_assoc. rewrite <- aux'.
-    rewrite add_comm. rewrite IHa. reflexivity.
-Qed.
-  
-(*Exercicios feitos por conta (Que nao estao do livro)*)
-
-Fixpoint sum (x : nat) : nat :=
-  match x with
-  |O => O
-  |S (x') => x + (sum x')
-  end.
-Fixpoint div2 (n : nat) : nat :=
-  match n with 
-  |O => O
-  |S O => O
-  |S(S n') => 1 + (div2 n')
-  end.
-
-Lemma double_dist: forall n b, double(n + b) = double(n) + double (b).
-Proof.
-  intros; induction n.
-  - simpl. reflexivity.
-  -  simpl. rewrite IHn. reflexivity.
-Qed.
-Lemma div_double: forall n, div2(double n) = n.
-Proof.
-  intros; induction n.
-  -simpl. reflexivity.
-  -simpl. rewrite IHn. reflexivity.
-Qed.
-Lemma mmc1: forall a b, div2(a) + b = div2(a + double(b)).
-Proof.
-  intros. induction b.
-  induction a.
-  - simpl. reflexivity.
-  - rewrite <- add_zero. replace (double 0) with 0.
-    rewrite <- add_zero. reflexivity.
-    +reflexivity.
-  - simpl.
-
-    rewrite <- add_zero.
-    reflexivity. 
-  - rewrite <- succ_n_plus_1. rewrite add_assoc. 
-    rewrite IHb. rewrite double_dist.
-    rewrite add_assoc. simpl.
+(*Exercicios feitos por conta (Que nao estao do livro)*) 
 
 Theorem gaussSum : forall n, (sum n) = div2 (n*(n+1)).
 Proof.
   intros. induction n.
   - simpl. reflexivity.
-  - rewrite mult_dist. rewrite <- succ_n_plus_1.
-    replace (sum (n+1)) with (sum(n) +(n + 1)).
-    + rewrite IHn. rewrite mult_com.
+  - rewrite mult_dist. rewrite -> sum_Sn. rewrite <- plus_n_1.
+    rewrite IHn.
+    rewrite div2_mult2_plus. simpl. rewrite add_0_r.
+    rewrite mult_dist. rewrite  mult_dist.
+    rewrite  <- mult_dist. rewrite add_comm. rewrite -> mul_comm.
+    rewrite -> add_assoc. reflexivity.
+Qed.
