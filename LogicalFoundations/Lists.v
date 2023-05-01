f3467c57ac51483391e50ddc14ae136ed93d5144
Pedro <pedrovargastannuri@gmail.com>
2023-05-01 16:36:53 -0300

    updated

--- LogicalFoundations/Lists.v
+++ LogicalFoundations/Lists.v
@@ -98,5 +98,64 @@ Fixpoint alternate (l1 l2 : natlist) : natlist :=
   end.
 (*Bags sao sets com repeticao*)
 Definition bag := natlist.
+(*Definindo algumas funcoes para os bags*)
 
+Fixpoint count (v : nat) (s : bag) : nat :=
+  match s with
+  |[] => O
+  |(h::t) => if (eqNat v h) then S (count v t)
+             else (count v t)
+  end.
+Example test_count1: count 1 [1;2;3;1;4;1] = 3.
+Proof.
+  reflexivity.
+Qed.
+Definition sum : bag -> bag -> bag :=
+  append.
 
+Example test_sum1: count 1 (sum [1;2;3] [1;4;1]) = 3.
+Proof.
+  reflexivity.
+Qed.
+Definition add (v : nat) (s : bag) : bag :=
+  sum s [v].
+Example test_add1: count 1 (add 1 [1;4;1]) = 3.
+Proof.
+  reflexivity.
+Qed.
+Example test_add2: count 5 (add 1 [1;4;1]) = 0.
+Proof.
+  reflexivity.
+Qed.
+
+Definition member (v : nat) (s : bag) : bool :=
+  if (eqNat (count v s) O) then false
+  else true.
+Fixpoint remove_one (v : nat) (s : bag) : bag :=
+  match s with
+  |[] => []
+  |(h::t) => if (eqNat v h) then t
+             else h :: (remove_one v t)
+  end.
+Fixpoint remove_all (v : nat) (s : bag) : bag :=
+  match s with
+  |[] => []
+  |(h::t) => if (eqNat v h) then remove_all v t
+             else h :: (remove_one v t)
+  end.
+
+Fixpoint included (s1 : bag) (s2 : bag) : bool :=
+  match s1,s2 with
+  |_,[] => false
+  |[], _ => true
+  |(h1::t1),(h2::t2) => if(eqNat(count h1 s2) 0) then false
+                        else included t1 (remove_one h1 s2)
+  end.
+ Example test_included1: included [1;2] [2;1;4;1] = true.
+Proof.
+  simpl. reflexivity.
+Qed.
+Example test_included2: included [1;2;2] [2;1;4;1] = false.
+Proof.
+  reflexivity.
+Qed.
