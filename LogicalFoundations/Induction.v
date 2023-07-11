6d82313f6bcd07734e2dab9597c8916038e004e0
Pedro <pedrovargastannuri@gmail.com>
2023-07-10 22:05:29 -0300

    arte de provar coisas

--- LogicalFoundations/Induction.v
+++ LogicalFoundations/Induction.v
@@ -2,6 +2,24 @@ From Coq Require Export String.
 From Coq Require Import Unicode.Utf8_core.
 (* Teoremas do Induction.v e Basics.v*)
 
+Lemma succ_def: ∀ a : nat,
+  S a = (a + 1).
+Proof.
+  intros.
+  induction a.
+  - reflexivity.
+  - simpl. rewrite IHa. reflexivity.
+Qed. 
+
+Lemma succ_def2: ∀ a : nat,
+  S a = (1 + a).
+Proof.
+  intros.
+  induction a.
+  - reflexivity.
+  - simpl. rewrite IHa. reflexivity.
+Qed. 
+
 Theorem add_0_r : forall n:nat, n + 0 = n.
 Proof.
   intros n. induction n as [| n' IHn'].
@@ -241,16 +259,20 @@ Definition eqBool (a b: bool): bool :=
   |_,_ => false
   end.
 
+Lemma false_equals_false:
+  false = false.
+Proof.
+  reflexivity.
+Qed.
+
 Lemma bool_diff_eq_false: forall a: bool,
-  a = false <-> a <> true.
+  (a <> false) -> (a = true).
 Proof.
   intros. destruct a.
-  - split.
-    + intro. discriminate.
-    + intro. destruct H. reflexivity.
-  - split.
-    + intro. discriminate.
-    + intro. reflexivity. 
+  - reflexivity.
+  - pose proof false_equals_false as H'.
+    apply H in H'.
+    destruct H'. 
 Qed.
 
 Theorem even_n_SSn: forall n : nat,
@@ -285,14 +307,13 @@ Proof.
     + destruct H. reflexivity.
     + reflexivity.
   - intro. destruct a.
-    + unfold not. intro. apply bool_diff_eq_false in H. destruct H.
-      reflexivity.
-    + apply bool_diff_eq_false in H. apply H.
+    + discriminate H.
+    + intro. discriminate H0.
 Qed.
 
 
 
-  Theorem even_n_odd: ∀ n: nat,
+Theorem even_n_odd: ∀ n: nat,
   even n = negb(odd n).
 Proof.
   intros.
@@ -368,7 +389,7 @@ Proof.
     apply H.
 Qed.
 
-Theorem auxx: ∀ A B: Prop,
+Theorem iff_neg: ∀ A B: Prop,
   (A <-> B) -> (~A <-> ~B).
 Proof.
   intros. destruct H.
@@ -442,6 +463,115 @@ Proof.
   - rewrite H. reflexivity.
 Qed. 
 
+Lemma div2_both_sides: ∀ a b : nat,
+  (a = b) -> ((div2 a) = (div2 b)).
+Proof.
+  intros.
+  induction a; induction b.
+  - reflexivity.
+  - rewrite H. reflexivity.
+  - rewrite H. reflexivity.
+  - rewrite H. reflexivity.
+Qed.
+
+Lemma add_sub_assoc: ∀ n : nat,
+  n + 1 - 1 = n + (1 - 1).
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl.  rewrite plus_n_Sm. change (S(n+0)-0) with (S(n+0)).
+    reflexivity.
+Qed.
+
+Lemma one_2_x: ∀ x: nat,
+   ~(1 = 2*x).
+Proof.
+  intros. intro. pose proof H as H'.
+  apply div2_both_sides in H.
+  rewrite <- mul2_div2 in H. simpl in H.
+  symmetry in H.
+  rewrite H in H'. simpl in H'. discriminate H'.
+Qed.
+ 
+Lemma mult_dist_sub: ∀ n : nat,
+  2*(n - 1) = 2*n - 2.
+Proof.
+  induction n.
+  - reflexivity.
+  - simpl. rewrite add_0_r. rewrite <- plus_n_Sm.
+    rewrite add_assoc. rewrite sub_0_r. rewrite add_sub_assoc. simpl(1-1).
+    rewrite add_0_r. reflexivity.
+Qed.
+
+Lemma minus_two_minus1_minus1: ∀ n : nat,
+  n - 2 = n - 1 - 1.
+Proof.
+  intros.
+  induction n.
+  - reflexivity.
+  - simpl. rewrite sub_0_r. reflexivity.
+Qed.
+
+Theorem n_2m_then_Sn_2m: ∀ n: nat,
+(∃ m : nat, S n = 2*m) -> ~(∃ m : nat, n = 2*m).
+Proof.
+  intros. induction n.
+  - destruct H. pose proof one_2_x as H'. intro.
+    destruct H' with x. apply H.
+  - intro. apply IHn in H0.
+    destruct H0.
+    destruct H.
+    exists (x-1). rewrite mult_dist_sub.
+    apply sub_one_both_sides in H.
+    apply sub_one_both_sides in H.
+    simpl in H. rewrite sub_0_r in H.
+    rewrite add_0_r in H.
+    simpl. rewrite add_0_r. rewrite minus_two_minus1_minus1. apply H.
+Qed.
+
+Lemma muda_nada:  ∀ a b c : nat,
+  (a = b) -> (a-c = b-c).
+Proof.
+  intros.
+  induction a; induction b.
+  - reflexivity.
+  - rewrite H. reflexivity.
+  - rewrite H. reflexivity.
+  - rewrite H. reflexivity.
+Qed.
+
+Theorem n_diff_Sn: ∀ n : nat,
+  n <> S n.
+Proof.
+  intro.
+  induction n.
+  - intro. discriminate H.
+  - intro. apply sub_one_both_sides in H.
+    simpl in H. rewrite sub_0_r in H. apply IHn in H.
+    destruct H.
+Qed.
+
+Theorem not_and_2m_2mplus1: ∀ n : nat,
+  (∃ m : nat, n = 2*m + 1) -> ~(∃ m : nat, n = 2*m).
+Proof.
+  intros.
+  intro.
+  destruct H. destruct H0.
+  pose proof H0 as H1.
+  rewrite H in H0.
+  apply div2_both_sides in H0.
+  rewrite <- mul2_div2 in H0.
+  rewrite <- div2_mult2_plus in H0.
+  simpl (div2 1) in H0.
+  rewrite add_0_r in H0.
+  rewrite H0 in H.
+  symmetry in H1.
+  rewrite H1 in H. rewrite <- succ_def in H.
+  apply n_diff_Sn in H. destruct H.
+Qed.
+
+
 
 Theorem even_classic_def: ∀ n: nat, 
   (even n = true) <-> (∃ m: nat, n = 2*m).
@@ -449,9 +579,9 @@ Proof.
   intros.
   induction n as [|k].
     + split.
-      -  exists 0. reflexivity.
+      - intro. exists 0. reflexivity.
       - intro. reflexivity.
-    + apply auxx in IHk as H'. split.
+    + apply iff_neg in IHk as H'. split.
       - intro. rewrite even_n_diff_true_even_Sn in H'. apply H' in H as H''.
         apply not_n_2m_then_n_2mplus1 in H''. 
         destruct H''. apply add_one_to_both_sides in H0.
@@ -461,5 +591,54 @@ Proof.
       - intro. destruct H'.
         pose proof nat_2m as H2.
         destruct H2 with k.
-        * 
+        * apply  n_2m_then_Sn_2m in H. apply H in H3. destruct H3.
+        * apply  not_and_2m_2mplus1 in H3. apply H1 in H3.
+          rewrite even_n_Sn in H3. rewrite bool_neg_both_sides in H3.
+          rewrite <- bool_dne in H3. simpl (negb true) in H3.
+          apply bool_diff_eq_false in H3. apply H3.
 Qed.
+
+
+Theorem odd_classic_def: ∀ n: nat, 
+  (odd n = true) <-> (∃ m: nat, n = 2*m + 1).
+Proof.
+  intros. induction n.
+  - split.
+    + intro. rewrite bool_neg_both_sides in H. rewrite <- even_n_odd in H.
+      simpl in H. discriminate H.
+    + intro. apply not_and_2m_2mplus1 in H. destruct H.
+      exists 0; reflexivity.
+  -
+    rewrite bool_neg_both_sides.
+    rewrite <- even_n_odd.
+    simpl(negb true).
+    destruct IHn.
+    split.
+    + intro. pose proof even_classic_def as H2.
+      destruct H2 with (S n).
+      apply contras in H4.
+      * apply not_n_2m_then_n_2mplus1 in H4.
+        apply H4.
+      * intro. rewrite H5 in H1. discriminate H1.
+    + intro. rewrite bool_neg_both_sides. rewrite <- even_n_Sn.
+      simpl (negb false). pose proof nat_2m as H2.
+      * destruct H2 with n.
+        ** apply even_classic_def in H3.
+           apply H3.
+        ** apply H0 in H3. apply H in H3. destruct H3. destruct H1.
+           pose proof H1 as H1'.
+           rewrite succ_def in H1'.
+           apply sub_one_both_sides in H1'.
+           rewrite add_sub_assoc in H1'.
+           simpl (1-1) in H1'. rewrite add_0_r in H1'.
+           pose proof H3 as H3'.
+           rewrite H1' in H3'.
+           rewrite add_sub_assoc in H3'.
+           simpl(1-1) in H3'. rewrite add_0_r in H3'.
+           apply div2_both_sides in H3'.
+           rewrite <- mul2_div2 in H3'. rewrite <- div2_mult2_plus in H3'.
+           simpl in H3'. rewrite add_0_r in H3'.
+           symmetry in H3. rewrite H3' in H1. rewrite H3 in H1.
+           pose proof n_diff_Sn as HMatadora.
+           destruct HMatadora with n. symmetry. apply H1.
+ Qed.
