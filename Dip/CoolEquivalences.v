e7b3a60a6d6ad04878f4bedc3e8b478ebb3dc673
pedrovargastannuri@gmail.com <pedrovargastannuri@gmail.com>
2023-10-12 11:50:32 -0300

    nice

--- Dip/CoolEquivalences.v
+++ Dip/CoolEquivalences.v
@@ -4,6 +4,8 @@ Definition DNe := forall A : Prop, ~~A -> A.
 
 Definition LEM := forall A : Prop, A \/ ~A.
 
+Definition IP := forall A : Prop, (~A -> False) -> A.
+
 Theorem pierce_lem_eq: Pierce <-> LEM.
 Proof.
   unfold Pierce, LEM. split.
@@ -38,5 +40,39 @@ Proof.
     { apply H0. } 
     { apply H in H0 as contra. destruct contra. }
 Qed.
+
+Theorem pierce_ip_eq: Pierce <-> IP.
+Proof.
+  unfold Pierce, IP. split.
+  - intro pierce. intro A. intro H.
+    apply pierce with (B := False). intro. unfold not in H.
+    apply H in H0 as contra. destruct contra.
+  - intro IP. intros A B. intro H.
+    apply IP. intro. apply H0. apply H. intro. apply H0 in H1 as contra.
+    destruct contra.
+Qed.
     
+Theorem lem_ip_eq: LEM <-> IP.
+Proof.
+  unfold LEM, IP. split.
+  {
+    intros lem. intro A. intro H. specialize lem with (A := A) as lem_spec.
+    destruct lem_spec.
+    - apply H0.
+    - apply H in H0 as contra. destruct contra.
+  }
+  {
+    intro ip. intro A. apply ip. intro. apply H. left. apply ip. intro. apply H.
+    right. apply H0.
+  }
+Qed.
+
+Theorem dne_ip_eq: DNe <-> IP.
+Proof.
+  unfold DNe, IP. split.
+  - intro dne. intro A. intro H.
+    apply dne. intro H0. apply H. apply H0.
+  - intro ip. intro A. intro H.
+    apply ip. intro H0. apply H. apply H0.
+Qed.
 
